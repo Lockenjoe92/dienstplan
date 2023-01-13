de9afe7e79418a386531f4b11ebc9f9f1baedb63
marchaefeker <marc@haefeker.de>
2023-01-13 20:15:37 +0100

    added site skeleton functions to dependencies.php

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,4 +1,11 @@
 <?php
 
 // This file is used to be able to access all functions in all scripts by simply calling one file
-include_once "db_config.php";   //Connection Settings to MySQL-Database
+//Connection Settings to MySQL-Database
+include_once "db_config.php";
+
+// Website Skeleton Functions
+include_once "./site_skeleton/site_skeleton.php";
+include_once "./site_skeleton/form_elements.php";
+include_once "./site_skeleton/table_elements.php";
+include_once "./site_skeleton/layout_elements.php";
