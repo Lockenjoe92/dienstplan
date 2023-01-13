a9b5288b631a24c9d47a25ad9f08966ea115c743
marchaefeker <marc@haefeker.de>
2023-01-13 20:13:18 +0100

    stared laying out website skeleton files and functions

---
+++ site_skeleton/form_elements.php
@@ -0,0 +1,15 @@
+<?php
+
+// Generates HTML-Header content if Session- & Permission-Manager doesn't trigger a redirect
+function site_header($loggedIn = false){
+
+
+
+}
+
+// Generates HTML-Body. Takes input-String and wraps it with the Footer & Skript commands
+function site_body($Content){
+
+
+
+}
