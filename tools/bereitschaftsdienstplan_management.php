f44a00c6503b0ce0cd9aad273f88777e79c79b99
marchaefeker <marc@haefeker.de>
2023-02-28 14:11:53 +0100

    scetched out bd_plan table

--- tools/bereitschaftsdienstplan_management.php
+++ tools/bereitschaftsdienstplan_management.php
@@ -1,1 +1,41 @@
 <?php
+
+function get_list_of_all_bd_types($mysqli, $ShowDeleted=false){
+
+    $BDtypes = [];
+
+    if($ShowDeleted){
+        $sql = "SELECT * FROM bereitschaftsdienst_typen ORDER BY rank, kuerzel ASC";
+    }else{
+        $sql = "SELECT * FROM bereitschaftsdienst_typen WHERE delete_time IS NULL ORDER BY reihung,kuerzel ASC";
+    }
+
+    if($stmt = $mysqli->query($sql)){
+        while ($row = $stmt->fetch_assoc()) {
+            $BDtypes[] = $row;
+        }
+    }
+
+    return $BDtypes;
+
+}
+
+function get_list_of_all_bd_matrixes($mysqli, $ShowDeleted=false){
+
+    $BDtypes = [];
+
+    if($ShowDeleted){
+        $sql = "SELECT * FROM bereitschaftsdienst_matrix";
+    }else{
+        $sql = "SELECT * FROM bereitschaftsdienst_matrix";
+    }
+
+    if($stmt = $mysqli->query($sql)){
+        while ($row = $stmt->fetch_assoc()) {
+            $BDtypes[] = $row;
+        }
+    }
+
+    return $BDtypes;
+
+}
